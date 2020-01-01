538852d4b7d847670c1182925916d2a4d6c67e70
Daniel Rumin <daniel.rumin@uth.pl>
2020-01-01 18:18:04 +0100

    Initial Commit

---
+++ Zadanie 3 v2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Zadanie 3 v2
+//
+//  Created by Daniel on 01/01/2020.
+//  Copyright © 2020 Daniel Rumin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
